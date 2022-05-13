0d8c09312ad2e9d7b7e910b5515e12d64d0749fb
Renata de Andrade <renata_r.a.s@hotmail.com>
2022-05-13 19:09:27 -0300

    Curso em Vídeo Gustavo Guanabara

---
+++ javascript/logica.js
@@ -0,0 +1,9 @@
+
+var nome = prompt('Qual o seu nome?')
+//alert('É um grande prazer te conhecer '+nome)
+alert(`${nome}, seu nome tem ${nome.length} letras. Seu nome escrito em maiúsculo é ${nome.toLocaleUpperCase()} e em minúsculo é ${nome.toLowerCase()}`)
+/*var n1 = Number.parseInt(prompt('Digite um número'))
+var n2 = Number.parseInt(prompt('Digite outro número'))
+var soma = n1+n2
+alert('A soma de '+n1+'+'+n2+' é igual a: '+soma)
+alert(`Testando template String. A soma de ${n1} + ${n2} é igual a ${soma}`)*/
